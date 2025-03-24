b283aaeb1edf1a78a974ebff6f35453aa222c3e8
TtheBC01 <tac688@alumni.stanford.edu>
2025-03-24 12:08:34 -0700

    cleanup

--- README.md
+++ README.md
@@ -1,25 +1,38 @@
 # Webhook Sinker
 
 This tiny repo implements a docker stack for locally testing webhooks. It uses [ngrok](https://ngrok.com) to establish an introspective
-tunnel from you local machine to a public URL which you can use as a webhook URL is various webhook services. 
+tunnel from you local machine to a public URL which you can use as a webhook URL for testing [1Shot API](https://1shotapi.com) webhook callbacks. 
 
+## 1. Fire Up the Docker Stack
 
-## Build the Python server
-
-First build the python server which will implement a route for your webhook:
-```
-docker build -t python-server -f src/python/Dockerfile
-```
-
-They [python server](./src/python/main.py) uses [fastapi](https://fastapi.tiangolo.com/). You can modify it as necessary for your purposes. 
-
-## Run the Docker Stack
-
-Next, grab your auth token from the ngrok dashboard and bring up the Docker webhook sinker stack:
+First, grab your auth token from the [ngrok dashboard](https://dashboard.ngrok.com/endpoints) and bring up the Docker stack:
 
 ```
-sudo NGROK_AUTHTOKEN=<my-auth-token> docker compose up -d
+sudo NGROK_AUTHTOKEN=<Your NGrok Token> docker compose up -d
 ```
 
-You can open [http://localhost:4040](http://localhost:4040) to see the ngrok dashboard to see the public URL assigned to your session and also
+You can open [http://localhost:4040](http://localhost:4040) to see the ngrok dashboard to see the **public URL** assigned to your session and also
 any requests made against it. 
+
+## 2. Create a 1Shot Transaction Endpoint
+
+On the [1Shot Endpoints page](https://app.1shotapi.com/endpoints), create a new endpoint. Here are some example values you can input:
+
+- Name: Sepolia TestToken Mint
+- Description: Mint free test tokens on the Sepolia Testnet
+- Blockchain: Sepolia
+- Webhook: You NGrok URL with the routhe `/python` appended to the end
+- Contract Address: []`0x17Ed2c50596E1C74175F905918dEd2d2042b87f3`](https://sepolia.etherscan.io/address/0x17Ed2c50596E1C74175F905918dEd2d2042b87f3)
+- Function Name: `mint`
+    - 1st parameter: `to` type `address`
+    - 2nd parameter: `amount` type `uint`
+
+## 3. Get the Webhook Public Key
+
+Once you've created the transaction endpoint, click on the details of the endpoint and copy the webhook's public key. Paste this in into the 
+demo server's [main.py](./src/python/main.py#L38) file. The fastAPI server will hot reload. 
+
+## 4. Trigger the Transaction Endpoint
+
+On the details page of the transaction you created, using the Input Parameters card to execute a transaction. Watch the ngrok tunnel inspector 
+for callbacks from 1Shot. You should see a `200 OK` message in a few seconds on the `/python` route. 
--- src/python/main.py
+++ src/python/main.py
@@ -6,32 +6,40 @@ from cryptography.exceptions import InvalidSignature
 
 app = FastAPI()
 
-async def verify_signature_decorator(request: Request):
-    try:
-        # Extract the required fields from the request
-        body = await request.json()  # Raw request body
-        signature = body.pop("signature", None)  # Example header name
-        
-        if not signature:
-            raise HTTPException(status_code=400, detail="Signature field missing")
-        
-        # Decode the signature
-        signature_bytes = base64.b64decode(signature)
-        
-        # Public key in base64 format
-        pubkey_b64 = "1N7gklNt5gpfMIHhIUR+csMIb7oO4JX7Q7QTdqj7+Qw="
-        # Load the public key
-        public_key = Ed25519PublicKey.from_public_bytes(
-            base64.b64decode(pubkey_b64)
-        )
-        # Verify the signature
-        public_key.verify(signature_bytes, json.dumps(body, separators=(',', ':'), sort_keys=True).encode('utf-8'))
-    except InvalidSignature:
-        raise HTTPException(status_code=403, detail="Invalid signature")
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Internal error: {e}")
+class webhookAuthenticator:
+    def __init__(self, pubkey_b64: str):
+        try:
+            self.public_key = Ed25519PublicKey.from_public_bytes(
+                                    base64.b64decode(pubkey_b64)
+                                )
+        except ValueError as e:
+            print(f"Error: {e}")
 
-@app.post("/python", dependencies=[Depends(verify_signature_decorator)])
+    async def __call__(self, request: Request):
+        try:
+            # Extract the required fields from the request
+            body = await request.json()  # Raw request body
+            signature = body.pop("signature", None)  # Example header name
+
+            if not signature:
+                raise HTTPException(status_code=400, detail="Signature field missing")
+
+            # Decode the signature
+            signature_bytes = base64.b64decode(signature)
+
+            # Verify the signature
+            self.public_key.verify(signature_bytes, json.dumps(body, separators=(',', ':'), sort_keys=True).encode('utf-8'))
+        except InvalidSignature:
+            raise HTTPException(status_code=403, detail="Invalid signature")
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Internal error: {e}")
+
+# Put your endpoint's webhook public key here:
+pubkey_base64 = "SBLzVF0dHNo/6tXo3+UOsYnCJ3Brq/SNxAFOAMWxTVo="
+
+myWebhook = webhookAuthenticator(pubkey_base64)
+
+@app.post("/python", dependencies=[Depends(myWebhook)])
 async def handle_python_webhook(request: Request):
     return {"message": "Webhook received and signature verified"}
 
