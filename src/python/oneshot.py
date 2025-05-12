f51fd906f004aa7f23805660430c23416ae4920a
TtheBC01 <tac688@alumni.stanford.edu>
2025-05-12 10:00:30 -0700

    added singleton import file

---
+++ src/python/oneshot.py
@@ -0,0 +1,13 @@
+import os
+
+from uxly_1shot_client import AsyncClient
+
+# its handy to set your API key and secret with environment variables so you only have to change them in one place (i.e. docker-compose.env)
+API_KEY = os.getenv("ONESHOT_API_KEY")
+API_SECRET = os.getenv("ONESHOT_API_SECRET")
+BUSINESS_ID = os.getenv("ONESHOT_BUSINESS_ID") 
+
+# import the the 1Shot API async client with your API key and secret from your 1Shot Org (https://docs.1shotapi.com/org-creation.html)  
+# its handy to instantiate it in a single location and import the singleton where you need it  
+# be sure to use the AsyncClient with asynchronous frameworks like FastAPI and python-telegram-bot       
+oneshot_client = AsyncClient(api_key=API_KEY, api_secret=API_SECRET)
